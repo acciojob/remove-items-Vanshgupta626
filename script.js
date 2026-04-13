76f8e7af8a31adf56d4c7ca46a533eaab885c6c8
Vansh gupta <vanshg6532@gmail.com>
2026-04-13 18:34:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// button ko select karo
+let button = document.querySelector("input[type='button']");
+
+// click event lagao
+button.addEventListener("click", function () {
+
+    let select = document.getElementById("colorSelect");
+
+    // selected option remove karo
+    if (select.selectedIndex !== -1) {
+        select.remove(select.selectedIndex);
+    }
+
+});
